7add306fca7124d0b2c86de0103ca9343c7da96a
André Felipe Carvalho dos Santos <felipecarvalhoacdm@gmail.com>
2025-04-04 17:49:15 -0300

    Update CrudApplication.java

--- src/main/java/com/example/crud/CrudApplication.java
+++ src/main/java/com/example/crud/CrudApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CrudApplication {
+	// Alterações GIT para JWT - Commit local
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
 	}
